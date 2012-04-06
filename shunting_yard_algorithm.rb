191518b6fbabce2735f33122764665513e6e6d6a
Sam Serpoosh <ssjesus287@gmail.com>
2012-04-06 23:55:42 +0430

    expression with multiple operators with same precedence(Refactored)

--- shunting_yard_algorithm.rb
+++ shunting_yard_algorithm.rb
@@ -1,27 +1,45 @@
 class ShuntingYardAlgorithm
 
+  def initialize
+    @output = ""
+    @current_operator = nil
+  end
+
+
   def post_fix(expression)
     return "" if expression.nil?
     return expression if expression.length <= 1
+
     tokens = expression.split(/ /)
-    output = ""
-    operator = nil
-    tokens.each do |token|
-      if is_number?(token)
-        output += token + " "
-      else
-        output += operator + " " unless operator.nil?
-        operator = token
-      end
-    end
-    output += operator 
-    output
+    process_tokens(tokens) 
+    append_token(@current_operator)
+
+    @output[0..-2]
   end
 
   private
+
+  def process_tokens(tokens)
+    tokens.each do |token|
+      if is_number?(token)
+        append_token(token)
+      else
+        append_operator(token)
+      end
+    end
+  end
+
   def is_number?(token)
-    number_pattern = /\d+/
-    !!(token =~ number_pattern)
+    !!(token =~ /\d+/)
+  end
+
+  def append_token(token)
+    @output += token + " "
+  end
+
+  def append_operator(token)
+    @output += @current_operator + " " unless @current_operator.nil?
+    @current_operator = token 
   end
 
 end
--- shunting_yard_algorithm_spec.rb
+++ shunting_yard_algorithm_spec.rb
@@ -3,30 +3,34 @@ require './shunting_yard_algorithm'
 describe ShuntingYardAlgorithm do
 
   it "returns empty for nil expression" do
-    subject.post_fix(nil).should == ""
+    algorithm.post_fix(nil).should == ""
   end
 
   it "does nothing for empty expression" do
-    subject.post_fix("").should == ""
+    algorithm.post_fix("").should == ""
   end
 
   it "does nothing for a single number" do
-    subject.post_fix("1").should == "1"
-    subject.post_fix("2").should == "2"
+    algorithm.post_fix("1").should == "1"
+    algorithm.post_fix("2").should == "2"
   end
 
   it "postfix simple add" do
-    subject.post_fix("1 + 1").should == "1 1 +"
-    subject.post_fix("2 + 2").should == "2 2 +"
+    algorithm.post_fix("1 + 1").should == "1 1 +"
+    algorithm.post_fix("2 + 2").should == "2 2 +"
   end
 
   it "postfix simple subtract" do
-    subject.post_fix("1 - 1").should == "1 1 -"
+    algorithm.post_fix("1 - 1").should == "1 1 -"
   end
 
   it "postfix expression with multiple operators with same precedence" do
-    subject.post_fix("1 + 2 + 3").should == "1 2 + 3 +"
-    subject.post_fix("1 - 2 + 3").should == "1 2 - 3 +"
+    algorithm.post_fix("1 + 2 + 3").should == "1 2 + 3 +"
+    algorithm.post_fix("1 - 2 + 3").should == "1 2 - 3 +"
+  end
+
+  def algorithm
+    ShuntingYardAlgorithm.new
   end
 
 end
